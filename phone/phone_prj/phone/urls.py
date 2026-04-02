153b9b13a72d20a4b6d8841a3c2c15ed3c9ead13
seobity <codemin1010@gmail.com>
2026-04-02 11:56:20 +0900

    [0402] 김민서

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -4,6 +4,10 @@ from .views import *
 app_name = 'phone'
 
 urlpatterns = [
-    path('', list, name='list'),
+    path('', ListView.as_view(), name='list'),
     path('result/', result, name='result'),
+    path('create/',create, name='create'),
+    path('detail/<int:id>/',detail,name='detail'),
+    path('update<int:id>/',update,name='update'),
+    path('delete<int:id>/',delete,name='delete'),
 ]
--- phone/phone_prj/phone/views.py
+++ phone/phone_prj/phone/views.py
@@ -1,12 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Phone
+from django.views.generic import *
 # Create your views here.
 
-def list(request):
-    phones = Phone.objects.all().order_by('name')
-    return render(request, 'phone/list.html',{'phones':phones})
+class ListView(ListView):
+    queryset = Phone.objects.all().order_by('name')
+    template_name = 'phone/list.html'
+    context_object_name = 'phones'
 
 def result(request):
     keyword = request.GET.get('keyword')
     results = Phone.objects.filter(name__contains=keyword).order_by('name')
     return render(request, 'phone/result.html', {'results': results, 'keyword': keyword})
+
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+        
+        phone = Phone.objects.create(
+            name = name,
+            phone_num = phone_num,
+            email = email
+        )
+        
+        return redirect('phone:list') # render와 redirect 차이
+    return render(request, 'phone/create.html')
+
+def detail(request, id):
+    phone = get_object_or_404(Phone, id=id)
+    return render(request, 'phone/detail.html', {'phone':phone})
+
+def update(request, id):
+    phone = get_object_or_404(Phone, id=id)
+    
+    if request.method == "POST":
+        phone.name = request.POST.get('name')
+        phone.phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+        phone.save()
+        return redirect('phone:detail',id)
+    
+    return render(request, 'phone/update.html', {'phone':phone})
+
+def delete(request, id):
+    phone= get_object_or_404(Phone, id=id)
+    if request.method == "POST":
+        phone.delete()
+        return redirect('phone:list')
+    return render(request, 'phone/delete.html',{'phone':phone})
